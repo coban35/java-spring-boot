1b18fbab6efefcca837969bb2f28e95c97d0bf09
mcoban <cobanfatih0635@gmail.com>
2023-03-31 15:36:08 +0300

    Add swagger

--- spring-boot-app/src/main/java/com/app/drone/controller/DroneController.java
+++ spring-boot-app/src/main/java/com/app/drone/controller/DroneController.java
@@ -3,6 +3,7 @@ package com.app.drone.controller;
 import com.app.drone.dto.DroneDTO;
 import com.app.drone.entity.Drone;
 import com.app.drone.service.DroneService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,31 +19,37 @@ public class DroneController {
     }
 
     @GetMapping(path = "/findAll")
+    @ApiOperation(value = "Tüm drone listesini getiren servis.")
     public List<DroneDTO> findAll() {
         return droneService.findAll();
     }
 
     @GetMapping(path = "/findById/{id}")
+    @ApiOperation(value = "Id'ye göre drone getiren servis.")
     public DroneDTO findById(@PathVariable("id") Long id) {
         return droneService.findById(id);
     }
 
     @PostMapping(path = "/save")
+    @ApiOperation(value = "Drone kaydeden servis.")
     public void save(@RequestBody Drone drone) {
         droneService.save(drone);
     }
 
     @PostMapping(path = "/saveAll")
+    @ApiOperation(value = "Drone toplu kaydeden servis.")
     public void saveAll(@RequestBody List<Drone> drone) {
         droneService.saveAll(drone);
     }
 
     @PostMapping("/update")
+    @ApiOperation(value = "Drone güncelleyen servis.")
     public void update(@RequestBody Drone drone) {
         droneService.update(drone);
     }
 
     @PostMapping(path = "/deleteById/{id}")
+    @ApiOperation(value = "Id'ye göre drone silen servis.")
     public void deleteById(@PathVariable("id") Long id) {
         droneService.deleteById(id);
     }
--- spring-boot-app/src/main/java/com/app/user/controller/UserController.java
+++ spring-boot-app/src/main/java/com/app/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.app.user.controller;
 import com.app.user.dto.UserDTO;
 import com.app.user.entity.User;
 import com.app.user.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,31 +19,37 @@ public class UserController {
     }
 
     @GetMapping(path = "/findAll")
+    @ApiOperation(value = "Tüm kullanıcı listesini getiren servis.")
     public List<UserDTO> findAll() {
         return userService.findAll();
     }
 
     @GetMapping("/findById/{id}")
+    @ApiOperation(value = "Id'ye göre kullanıcı getiren servis.")
     public UserDTO findById(@PathVariable("id") Long id) {
         return userService.findById(id);
     }
 
     @PostMapping("/save")
+    @ApiOperation(value = "Kullanıcı kaydeden servis.")
     public void save(@RequestBody User user) {
         userService.save(user);
     }
 
     @PostMapping("/saveAll")
+    @ApiOperation(value = "Kullanıcı toplu kaydeden servis.")
     public void saveAll(@RequestBody List<User> userList) {
         userService.saveAll(userList);
     }
 
     @PostMapping("/update")
+    @ApiOperation(value = "Kullanıcı güncelleyen servis.")
     public void update(@RequestBody User user) {
         userService.update(user);
     }
 
     @PostMapping("/deleteById/{id}")
+    @ApiOperation(value = "Id'ye göre kullanıcı silen servis.")
     public void deleteById(@PathVariable("id") Long id) {
         userService.deleteById(id);
     }
